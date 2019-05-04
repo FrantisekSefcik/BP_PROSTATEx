88870905d180b86990846b2a2b18e4481c05abbf
František Šefčík <frantisek.sefcik@gmail.com>
2019-05-04 20:26:39 +0200

    refactor and comments model

--- python/siamese/model.py
+++ python/siamese/model.py
@@ -9,43 +9,77 @@ flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 def mnist_model(input, reuse=False):
+	"""
+	CNN model taken from https://github.com/ardiya/siamesenetwork-tensorflow
+	"""
 	with tf.name_scope("model"):
 		with tf.variable_scope("conv1") as scope:
 			net = tf.contrib.layers.conv2d(input, 32, [7, 7], activation_fn=tf.nn.relu, padding='SAME',
-		        weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
+				weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
 			net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
 
 		with tf.variable_scope("conv2") as scope:
 			net = tf.contrib.layers.conv2d(net, 64, [5, 5], activation_fn=tf.nn.relu, padding='SAME',
-		        weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
+				weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
 			net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
 
 		with tf.variable_scope("conv3") as scope:
 			net = tf.contrib.layers.conv2d(net, 128, [3, 3], activation_fn=tf.nn.relu, padding='SAME',
-		        weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
+				weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
 			net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
 
 		with tf.variable_scope("conv4") as scope:
 			net = tf.contrib.layers.conv2d(net, 256, [1, 1], activation_fn=tf.nn.relu, padding='SAME',
-		        weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
+				weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
 			net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
 
 		with tf.variable_scope("conv5") as scope:
-			net = tf.contrib.layers.conv2d(net, 2, [1, 1], activation_fn=None, padding='SAME',
-		        weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
+			net = tf.contrib.layers.conv2d(net, 8, [1, 1], activation_fn=None, padding='SAME',
+				weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
 			net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
 
 		net = tf.contrib.layers.flatten(net)
 	
 	return net
 
-
-def xmas_model(input, reuse=False):
+def xmas_model3d(input, reuse=False):
 
 	with tf.name_scope("model"):
 		with tf.variable_scope("conv1") as scope:
+			net = tf.contrib.layers.conv3d(input, 32, 3, activation_fn=tf.nn.relu, padding='SAME',
+				scope=scope,reuse=reuse)
+
+		with tf.variable_scope("conv2") as scope:
+			net = tf.contrib.layers.conv3d(net, 32, 3, activation_fn=tf.nn.relu, padding='SAME',
+				scope=scope,reuse=reuse)
+			net = tf.contrib.layers.max_pool3d(net, 2, padding='SAME')
+
+		with tf.variable_scope("conv3") as scope:
+			net = tf.contrib.layers.conv3d(net, 64, 3, activation_fn=tf.nn.relu, padding='SAME',
+				scope=scope,reuse=reuse)
+
+		with tf.variable_scope("conv4") as scope:
+			net = tf.contrib.layers.conv3d(net, 64, 3, activation_fn=tf.nn.relu, padding='SAME',
+				scope=scope,reuse=reuse)
+			net = tf.contrib.layers.max_pool3d(net, 2, padding='SAME')
+
+		with tf.variable_scope("fc1") as scope:
+			net = tf.contrib.layers.flatten(net)
+			net = tf.contrib.layers.fully_connected(net, 256, activation_fn=tf.nn.relu, scope=scope, reuse=reuse)
+
+		with tf.variable_scope("fc2") as scope:
+			net = tf.contrib.layers.fully_connected(net, 8, activation_fn=tf.nn.relu, scope=scope, reuse=reuse)
+	
+	return net
+
+def xmas_model(input, reuse=False):
+	"""
+	CNN model inspired by: Liu, Saifeng et al. “Prostate cancer diagnosis using deep learning with 3D multiparametric MRI.” Medical Imaging:    Computer-Aided Diagnosis (2017).
+	"""
+	with tf.name_scope("model"):
+		with tf.variable_scope("conv1") as scope:
 			net = tf.contrib.layers.conv2d(input, 32, [3, 3], activation_fn=tf.nn.relu, padding='SAME',
-		        weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
+				weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
 
 		with tf.variable_scope("conv2") as scope:
 			net = tf.contrib.layers.conv2d(net, 32, [3, 3], activation_fn=tf.nn.relu, padding='SAME',
@@ -61,25 +95,19 @@ def xmas_model(input, reuse=False):
 		        weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
 			net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
 
-		with tf.variable_scope("conv5") as scope:
-			net = tf.contrib.layers.conv2d(net, 256, [1, 1], activation_fn=tf.nn.relu, padding='SAME',
-		        weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
-			net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
+		with tf.variable_scope("fc1") as scope:
+			net = tf.contrib.layers.flatten(net)
+			net = tf.contrib.layers.fully_connected(net, 256, activation_fn=tf.nn.relu, scope=scope, reuse=reuse)
 
-		with tf.variable_scope("conv6") as scope:
-			net = tf.contrib.layers.conv2d(net, 2, [1, 1], activation_fn=None, padding='SAME',
-		        weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),scope=scope,reuse=reuse)
-			net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
-
-		net = tf.contrib.layers.flatten(net)
+		with tf.variable_scope("fc2") as scope:
+			net = tf.contrib.layers.fully_connected(net, 8, activation_fn=tf.nn.relu, scope=scope, reuse=reuse)
 	
 	return net
-
 
 def contrastive_loss(model1, model2, y, margin):
 	with tf.name_scope("contrastive-loss"):
 		distance = tf.sqrt(tf.reduce_sum(tf.pow(model1 - model2, 2), 1, keepdims=True))
-		similarity = y * tf.square(distance)                                           # keep the similar label (1) close to each other
-		dissimilarity = (1 - y) * tf.square(tf.maximum((margin - distance), 0))        # give penalty to dissimilar label if the distance is bigger than margin
-
+		similarity = y * tf.square(distance)  # keep the similar label (1) close to each other
+		# give penalty to dissimilar label if the distance is bigger than margin
+		dissimilarity = (1 - y) * tf.square(tf.maximum((margin - distance), 0))        
 		return tf.reduce_mean(dissimilarity + similarity) / 2
